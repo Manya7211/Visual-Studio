cc12a3172601695aa13a1e63fd8611f73d88aeeb
Manya7211 <83996642+Manya7211@users.noreply.github.com>
2021-08-29 21:06:52 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,4 @@
-menu_list_array = ["Chicken Tandoori Pizza","Deluxe Veggie Pizza","Panee Tikka Pizza","Veg Extravaganza Pizza"];
-
-function getmenu(){
-var htmldata;
-menu_list_array.sort();
-        for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+ menu_list_array[i] + '<br>'
-        }
-        document.getElementById("display_menu").innerHTML = htmldata;  
-}
-
-function add_item(){
-var htmldata;
-var item=document.getElementById("add_item").value;
-menu_list_array.sort();
-        htmldata=""
-        for(var i=0;i<menu_list_array.length;i++){
-            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
-        }
-         document.getElementById("display_addedmenu").innerHTML = htmldata;		
-      }
-
-function add_toppings(){
-    var item=document.getElementById("add_item").value;
-    menu_list_array.push(item);
-	add_item();
+var guest_list_array = [];
+function submit() {
+        guest_list_array.push(guest_list_array);
 }
